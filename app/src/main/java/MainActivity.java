2deb71539df6e0c20315b01415d1bc8aa1c5f3fb
unisocsu <rmk0557603971@outlook.com>
2026-07-01 20:23:50 +0300

    Update MainActivity.java

--- app/src/main/java/MainActivity.java
+++ app/src/main/java/MainActivity.java
@@ -1,160 +1,31 @@
-package com.example.shellrunner;
+<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools">
 
-import androidx.appcompat.app.AppCompatActivity;
+    <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
+    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" android:maxSdkVersion="28" />
+    <uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE" tools:ignore="ScopedStorage" />
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Environment;
-import android.provider.Settings;
-import android.view.KeyEvent;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.ScrollView;
-import android.widget.TextView;
+    <uses-permission android:name="android.permission.INTERNET" />
+    <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+    <uses-permission android:name="android.permission.FOREGROUND_SERVICE" />
 
-public class MainActivity extends AppCompatActivity {
+    <application
+        android:allowBackup="true"
+        android:icon="@android:drawable/sym_def_app_icon"
+        android:label="Shell Runner"
+        android:theme="@style/Theme.AppCompat.NoActionBar" android:requestLegacyExternalStorage="true">
 
-    private TextView tvCurrentPath;
-    private ListView fileListView;
-    private RadioButton rbInternal;
-
-    private AppExplorer appExplorer;
-    private TerminalManager terminalManager;
-    private ScriptExecutor scriptExecutor;
-    private AutoRunManager autoRunManager;
-
-    private List<File> fileList = new ArrayList<>();
-    private List<String> fileNames = new ArrayList<>();
-    private ArrayAdapter<String> adapter;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // א) אתחול רכיבי UI
-        tvCurrentPath = findViewById(R.id.tvCurrentPath);
-        fileListView = findViewById(R.id.fileListView);
-        rbInternal = findViewById(R.id.rbInternal);
+        <activity 
+            android:name=".MainActivity"
+            android:exported="true"
+            android:configChanges="orientation|screenSize|keyboardHidden|keyboard">
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN" />
+                <category android:name="android.intent.category.LAUNCHER" />
+            </intent-filter>
+        </activity>
         
-        ScrollView terminalScrollView = findViewById(R.id.terminalScrollView);
-        TextView tvTerminalOutput = findViewById(R.id.tvTerminalOutput);
-
-        // אתחול מחלקות עזר
-        terminalManager = new TerminalManager(this, tvTerminalOutput, terminalScrollView);
-        scriptExecutor = new ScriptExecutor(this, terminalManager);
-        appExplorer = new AppExplorer(this, tvCurrentPath, fileList, fileNames);
-        autoRunManager = new AutoRunManager(this);
-
-        // הגדרת ה-Adapter לסייר
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, fileNames);
-        fileListView.setAdapter(adapter);
-
-        // מאזין ללחיצות רגילות (גיבוי למקשים)
-        fileListView.setOnItemClickListener((parent, view, position, id) -> handleSelection(position));
-
-        // בדיקת הרשאות וטעינה
-        checkStoragePermissions();
-
-        // ה) הרצה אוטומטית שקטה בהפעלה
-        autoRunManager.runBootScriptIfConfigured();
-    }
-
-    private void checkStoragePermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            if (!Environment.isExternalStorageManager()) {
-                try {
-                    Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-                    intent.setData(Uri.parse("package:" + getPackageName()));
-                    startActivityForResult(intent, 101);
-                } catch (Exception e) {
-                    Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
-                    startActivityForResult(intent, 101);
-                }
-            } else {
-                appExplorer.loadDirectory(Environment.getExternalStorageDirectory(), adapter);
-            }
-        } else {
-            if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, 102);
-            } else {
-                appExplorer.loadDirectory(Environment.getExternalStorageDirectory(), adapter);
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 101 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && Environment.isExternalStorageManager()) {
-            appExplorer.loadDirectory(Environment.getExternalStorageDirectory(), adapter);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 102 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            appExplorer.loadDirectory(Environment.getExternalStorageDirectory(), adapter);
-        }
-    }
-
-    // ג) ניתוב ההרצה לפי בחירת המשתמש
-    private void handleSelection(int position) {
-        File selectedFile = fileList.get(position);
-        if (selectedFile.isDirectory()) {
-            appExplorer.loadDirectory(selectedFile, adapter);
-        } else {
-            if (rbInternal.isChecked()) {
-                scriptExecutor.executeInInternalTerminal(selectedFile.getAbsolutePath());
-            } else {
-                scriptExecutor.executeInExternalTerminal(selectedFile.getAbsolutePath());
-            }
-        }
-    }
-
-    // ניהול מקשים פיזיים
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        int currentPosition = fileListView.getSelectedItemPosition();
-
-        switch (keyCode) {
-            case KeyEvent.KEYCODE_DPAD_UP:
-                if (currentPosition > 0) fileListView.setSelection(currentPosition - 1);
-                return true;
-            case KeyEvent.KEYCODE_DPAD_DOWN:
-                if (currentPosition < fileList.size() - 1) fileListView.setSelection(currentPosition + 1);
-                return true;
-            case KeyEvent.KEYCODE_DPAD_CENTER:
-            case KeyEvent.KEYCODE_ENTER:
-                if (currentPosition != ListView.INVALID_POSITION) handleSelection(currentPosition);
-                return true;
-            case KeyEvent.KEYCODE_BACK:
-                File parent = appExplorer.getCurrentDir().getParentFile();
-                if (parent != null && !appExplorer.getCurrentDir().getAbsolutePath().equals(Environment.getExternalStorageDirectory().getAbsolutePath())) {
-                    appExplorer.loadDirectory(parent, adapter);
-                    return true;
-                }
-                break;
-            case KeyEvent.KEYCODE_SPACE:
-            case KeyEvent.KEYCODE_S:
-                if (currentPosition != ListView.INVALID_POSITION) {
-                    File selectedFile = fileList.get(currentPosition);
-                    if (!selectedFile.isDirectory()) {
-                        autoRunManager.saveScriptForAutoRun(selectedFile.getAbsolutePath());
-                    }
-                }
-                return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-}
+    </application>
+</manifest>
